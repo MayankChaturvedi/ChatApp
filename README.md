f88725552d6760e49f984272e057c768514ff742
Mayank Chaturvedi <chaturvedim4@gmail.com>
2021-06-20 22:56:34 +0530

    added uploading feature

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Start the node.js server by running node index.js
 Access the chatroom at port 300 or use a tunneling solution like ngrok for global access
 
 If you use ngrok, this command might help - ./ngrok http -host-header=rewrite 3000
+
+Resources: https://socket.io/get-started/chat - socket.io chat
+
--- index.js
+++ index.js
@@ -5,14 +5,34 @@ const server = http.createServer(app);
 const {Server} = require("socket.io");
 const io = new Server(server);
 
+const upload = require('express-fileupload');
+
+app.use(upload());
 app.get('/', (req, res) => {
     res.sendFile(__dirname+'/index.html');
 });
 
+app.post('/', function(req, res) {
+    if(req.files){
+        var file = req.files.file;
+        var filename = file.name;
+        file.mv('/Users/mayankchaturvedi/Development/ChatApp/ChatApp/uploads/'+filename,function(err){
+            if(err){
+                res.send("Upload failed, you might not have the required permission");
+            }else{
+                res.send("Upload Successful");
+            }
+        });
+    }
+    else{
+        res.send("No file received");
+    }
+});
+
 var connections = {};
 io.on('connection', (socket) => {
-    var id = socket.id;
-    connections[id] = id;
+    var id = Object.keys(connections).length;
+    connections[id] = 'User'+id;
     socket.broadcast.emit('chat message','new user joined default name : '+connections[id]);
     socket.on('chat message', (msg) => {
         socket.broadcast.emit('chat message',connections[id] +' : '+ msg);
